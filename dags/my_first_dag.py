f957325ea58fb03f718e497fc5cdbf113660a9b8
Daniel Rondon <rondondaniel@gmail.com>
2025-04-22 18:55:20 +0200

    Init Commit

---
+++ dags/my_first_dag.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+
+# A DAG represents a workflow, a collection of tasks
+with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
+    # Tasks are represented as operators
+    hello = BashOperator(
+        task_id="hello",
+        bash_command="echo Hello from Bash"
+    )
+
+    @task()
+    def airflow():
+        print(datetime.now())
+        print("Hello from Airflow!")
+
+    # Set dependencies between tasks
+    hello >> airflow()

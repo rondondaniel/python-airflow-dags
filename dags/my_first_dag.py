6d8530de3011076441399c5783054329673098a8
Daniel Rondon <rondondaniel@gmail.com>
2025-05-13 14:33:02 +0200

    [Feat] add python operator

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 
 # A DAG represents a workflow, a collection of tasks
 with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") as dag:
@@ -12,8 +13,17 @@ with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="0 0 * * *") a
         bash_command="echo Hello from Bash"
     )
 
+    def new_airflow():
+        print(datetime.now())
+        print("Hello from Airflow python operator!")
+
+    t2 = PythonOperator(
+        task_id="airflow",
+        python_callable=new_airflow
+    )
+
     @task()
-    def airflow():
+    def new_airflow():
         print(datetime.now())
         print("Hello from Airflow!")
 
